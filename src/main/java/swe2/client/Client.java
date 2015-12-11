bead1c7d4ef645a0e16d5b3cc9ef7a247eeee4ab
ChicknDuck <antonkolb@users.noreply.github.com>
2015-12-11 13:26:55 +0100

    Coden (Zeitaufwand: 0.2h) - Client (vorraussichtlich ) fertig

--- src/main/java/swe2/client/Client.java
+++ src/main/java/swe2/client/Client.java
@@ -1,13 +1,21 @@
 package swe2.client;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.Socket;
+import swe2.shared.model.DataPackage;
+import swe2.shared.model.RequestType;
 
 class Client{
 	
 	ObjectInputStream reader; ObjectOutputStream writer;
 	String serverName; int serverPort;
 	Socket socket;
+        
+        public Client(){
+            this.serverName = "localhost";
+            this.serverPort = 6666;
+        }
 
 	public Client( String serverName, int serverPort ){
 	
@@ -26,18 +34,64 @@ class Client{
 		reader = new ObjectInputStream( socket.getInputStream() );
 		
 		System.out.println( "Stream erstellt" );
-		writer.writeObject( "Test" );
+		//writer.writeObject( "Test" );
 		
 	}//connect
 	
 	public void close() throws Exception{
-	
 		socket.close();
-	
 	}
-	
-	public void doStuff() throws Exception{
-		writer.writeObject( "Test" );
+        
+        /*-----------------------------------------
+        * MAIN METHODS
+        * ---------------------------------------*/
+        
+        public Serializable getCombustions() throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.GET_COMBUSTION, "Need Combustion" ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           return (Serializable) returnedData.getData();	
 	}
+        
+        public Serializable getDeliveries() throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.GET_DELIVERY, "Need Delivery" ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           return (Serializable) returnedData.getData();	
+	}
+        
+        public Serializable getWasteStorage() throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.GET_WASTESTORAGE, "Need WasteStorage" ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           return (Serializable) returnedData.getData();	
+	}
+        
+	public Serializable grantAccess( Serializable data ) throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.PUT_DELIVERY, data ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           if(  returnedData.getRequestType() == RequestType.GRANTED )
+               return (Serializable) returnedData.getData();
+           else
+               System.out.println( returnedData.getData() );
+               return null;	
+	}
+        
+	public String saveCombustion( Serializable data ) throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.PUT_COMBUSTION, data ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           return (String) returnedData.getData();	
+	}
+        
+        public String saveDelivery( Serializable data ) throws Exception{
+            
+           writer.writeObject( new DataPackage( RequestType.PUT_DELIVERY, data ) );
+           DataPackage returnedData = (DataPackage) reader.readObject();
+           return (String) returnedData.getData();	
+	}
+        
+
 
 }//class
