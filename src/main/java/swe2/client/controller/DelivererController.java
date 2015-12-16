92c148d71d7bd3ec023e6d41865c0d65922aa42b
ChicknDuck <antonkolb@users.noreply.github.com>
2015-12-16 16:31:56 +0100

    Coden (Zeitaufwand: 2.0h) - Deliverer (Fortsetzung)

--- src/main/java/swe2/client/controller/DelivererController.java
+++ src/main/java/swe2/client/controller/DelivererController.java
@@ -5,17 +5,21 @@
  */
 package swe2.client.controller;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import java.util.regex.Pattern;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import swe2.client.ClientConnection;
 import swe2.client.deliverer.Main;
+import swe2.shared.model.Deliverer;
+import swe2.shared.model.Delivery;
+import swe2.shared.model.UniformWaste;
 import swe2.shared.model.User;
+import swe2.shared.model.WasteAmount;
+import swe2.shared.model.WasteType;
 
 /**
  *
@@ -25,20 +29,20 @@ public class DelivererController {
     
     private Stage delivererStage;
     private Main main;
-    private User loggedInUser;
+    private Deliverer loggedInUser;
     private ClientConnection client;
     
     @FXML
     TextField idField, amountField;
     @FXML
-    SplitMenuButton typeSelect;
-    @FXML
-    Label typeLabel;
+    ChoiceBox typeSelect;
     @FXML
     Button submitButton;
     
     @FXML
     public void initialize() {
+        
+        typeSelect.setItems( FXCollections.observableArrayList("Paper", "Plastic", "Residual") );
         
         /*
         typeSelect.setOnAction( new EventHandler() {
@@ -49,25 +53,73 @@ public class DelivererController {
         });
                 */
     }
-    
-    public void selectType(){
-        
-        System.out.println( "ggg" + typeSelect.getText() ) ;
-        typeLabel.setText( typeSelect.getText() );
-    }
+   
     
     public void saveDelivery(){
-    
-    
+        
+        String error = "";
+        
+        if( typeSelect.getValue() == null ){
+            error += "Es wurde kein Typ ausgewaehlt!\n";
+        }
+        if( amountField.getText().equals("") ){
+            error += "Es wurde keine Menge angegeben!\n";
+        }/*
+        else if( Pattern.matches("[a-zA-Z]+", amountField.getText() ) == true ){
+            error += "Nur Zahlen bei Menge (kein kg, etc.)!\n";
+        }*/
+        
+        if( ! (error.equals("")) ){
+            main.showError( error );
+        }else{
+            
+            WasteType type = getWasteType( (String) typeSelect.getValue() );
+            WasteAmount amount = null;
+            try{
+                amount = new WasteAmount( Integer.parseInt( amountField.getText() ) ) ;
+            }catch( IllegalArgumentException e ){
+                e.printStackTrace();
+                main.showError( "Nur Zahlen bei Menge (kein kg, etc.)!" );
+                return;
+            }
+            
+            try{
+            String feedback = client.saveDelivery(
+                        new Delivery(
+                                new UniformWaste(
+                                        type,
+                                        amount 
+                                ),
+                                loggedInUser
+                        )
+                );
+            }catch( Exception e ){
+                e.printStackTrace();
+                main.showError( "Probleme beim Speichern" );
+            }
+        }
+        
     }
     
+    /*
+    * Compare Text and decide
+    * It can only be one of the three options because it's a choice box
+    */
+    private WasteType getWasteType( String inputFromChoice ){
+        if( inputFromChoice.equals("Paper") )
+            return WasteType.PAPER;
+        else if( inputFromChoice.equals("Plastic") )
+            return WasteType.PLASTIC;
+        else
+            return WasteType.RESIDUAL;
+    }
     
     public void setMain( Main main ){
         this.main = main;
     }
     
     public void setLoggedInUser( User user ){
-        this.loggedInUser = user;
+        this.loggedInUser = (Deliverer) user;
         idField.setText( loggedInUser.getId() );
     }
     
