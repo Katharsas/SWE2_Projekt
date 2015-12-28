0c3bb9bdc1e632274a4905c8523c07a1c4879a24
Katharsas <j.mothes@gmx.net>
2015-12-28 19:58:39 +0100

    Revert "Coden: Operator View Delivery Reports (1h): Funktion ist aufrufbar. Weitere Fenster einbindbar."
    This reverts commit 201382aa82f78e7468f27de4aaa99b64136aefcd.

--- src/main/java/swe2/client/controller/OperatorController.java
+++ src/main/java/swe2/client/controller/OperatorController.java
@@ -7,129 +7,41 @@ package swe2.client.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import swe2.client.ClientConnection;
-import swe2.client.controller.operator.OperatorDeliveryController;
-import swe2.client.deliverer.Main;
-import swe2.shared.model.Operator;
-import swe2.shared.model.User;
+import swe2.client.net.ClientConnection;
 
 /**
  * Controller for the menumask of the operator
- * 
  * @author pmarek
  */
-public class OperatorController {
+public class OperatorController extends Controller {
 
-	private Stage operatorStage;
-	private swe2.client.operator.Main main;
-	private User loggedInUser;
 	private ClientConnection client;
 	private ObservableList<String> options = FXCollections.observableArrayList(
-			"View Delivery Reports", "View Waste Storage", "Create Combustion",
-			"End Combustion", "View Combustion Reports");
+				"View Delivery Reports",
+				"View Waste Storage",
+				"Create Combustion",
+				"End Combustion",
+				"View Combustion Reports"
+			);
+			
 
 	@FXML
-	TextField idField, amountField;
+	TextField amountField;
 	@FXML
 	ComboBox<String> cBoxTask;
 	@FXML
 	Button submitButton;
-
+	
 	/**
 	 * Initializes the comboxbox for further use
 	 */
 	@FXML
 	public void initialize() {
 		cBoxTask.setItems(options);
-	}
-
-	public void setMain(swe2.client.operator.Main main) {
-		this.main = main;
-	}
-
-	public void setLoggedInUser(User user) {
-		this.loggedInUser = user;
-		idField.setText(loggedInUser.getId());
-	}
-
-	public void setOperatorStage(Stage operatorStage) {
-		this.operatorStage = operatorStage;
-	}
-
-	@FXML
-	public void submit() {
-		Stage operatorDeliveryStage;
-		AnchorPane operatorDelivery;
-		OperatorDeliveryController ctrl;
-		FXMLLoader loader;
-		try {
-			loader = getNextStage(cBoxTask.getSelectionModel()
-					.getSelectedItem());
-			operatorDelivery = loader.load();
-			operatorDeliveryStage = new Stage(); // Muss initialisiert werden
-
-			operatorDeliveryStage.initModality(Modality.WINDOW_MODAL);
-			operatorDeliveryStage.initOwner(operatorStage);
-
-			operatorDeliveryStage.setScene(new Scene(operatorDelivery));
-
-			operatorDeliveryStage.getScene().getWindow()
-					.setOnCloseRequest(ev -> {
-						operatorStage.close();
-						ev.consume();
-						System.out.println("Exit");
-
-					});
-
-			ctrl = loader.getController();
-			ctrl.setOperatorTaskStage(operatorDeliveryStage);
-			operatorDeliveryStage.setTitle("View Delivery Reports");
-
-			operatorDeliveryStage.showAndWait();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private FXMLLoader getNextStage(String selectedItem) {
-		FXMLLoader loader = new FXMLLoader(
-				Main.class
-						.getResource("/view/operator_view_delivery_reports.fxml"));
-		switch (selectedItem) {
-		case "View Delivery Reports":
-			loader = new FXMLLoader(
-					Main.class
-							.getResource("/view/operator_view_delivery_reports.fxml"));
-			break;
-		case "View Waste Storage":
-			loader = new FXMLLoader(
-					Main.class.getResource("/view/mssg_win.fxml"));
-			break;
-		case "Create Combustion":
-			break;
-		case "End Combustion":
-			break;
-		case "View Combustion Reports":
-			break;
-		default:
-			new FXMLLoader(
-					Main.class
-							.getResource("/view/operator_view_delivery_reports.fxml"));
-		}
-		return loader;
 	}
 
 }
--- src/main/java/swe2/client/operator/Main.java
+++ src/main/java/swe2/client/operator/Main.java
@@ -5,7 +5,7 @@
  */
 package swe2.client.operator;
 
-import javafx.application.Application;
+/*import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,13 +18,14 @@ import swe2.client.controller.OperatorController;
 import swe2.client.controller.LoginControllerOperator;
 import swe2.shared.model.Operator;
 import swe2.shared.model.User;
+*/
 
 /**
  *
  * @author pmarek
  */
 
-public class Main extends Application {
+/*public class Main extends Application {
 
 	private Stage primaryStage;
 	private AnchorPane root;
@@ -35,7 +36,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Müllverbrenner 2000 Express");
+		this.primaryStage.setTitle("Muellverbrenner 2000 Express");
 		FXMLLoader loader = null;
 
 		try {
@@ -49,8 +50,8 @@ public class Main extends Application {
 		}
 
 		ctrl = loader.getController();
-		ctrl.setOperatorStage(this.primaryStage);
-		ctrl.setMain(this);
+		//ctrl.setOperatorStage(this.primaryStage);
+		//ctrl.setMain(this);
 
 		showLoginMask(this.primaryStage);
 
@@ -80,7 +81,7 @@ public class Main extends Application {
 
 			});
 			loginStage.setTitle("Anmeldung");
-			
+
 			ctrl = loader.getController();
 			ctrl.setLoginStage(loginStage);
 			ctrl.setMain(this);
@@ -121,4 +122,4 @@ public class Main extends Application {
 		this.loggedInUser = user;
 	}
 
-}
+}*/
